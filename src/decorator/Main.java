63b781207ee9e7577651599726137c21a1e62252
Ahmed Essam <ahmedessam221298@gmail.com>
2020-11-04 20:58:33 +0200

    Test some corner cases.

--- src/decorator/Main.java
+++ src/decorator/Main.java
@@ -4,11 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Bevarage bevarage = new Espresso();
         System.out.println(bevarage.getDescription()+" : "+bevarage.cost()+"$");
-        /*   Bevarage bevarage1 = new DarkRoast();
+           Bevarage bevarage1 = new DarkRoast();
              bevarage1 = new Mocha(bevarage1);
-             // wrapper
-      */
-        Bevarage bevarage2 = new Mocha(new Espresso()); // another way to make wrapper
-        System.out.println(bevarage2.getDescription()+" : "+bevarage2.cost()+"$");
+             bevarage1 = new Mocha(bevarage1);
+             bevarage1 = new Milk(bevarage1);
+             // wrapping a lot of decorators
+
+       // Bevarage bevarage2 = new Mocha(new Espresso()); // another way to make it wrapped
+       // bevarage2 = new Mocha(new Espresso());
+        System.out.println(bevarage1.getDescription()+" : "+(float)bevarage1.cost()+"$");
     }
 }
